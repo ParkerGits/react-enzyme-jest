ff064c9a9cf87cc726d22ffd30735cd312c239a0
Parker <parkerjlandon@gmail.com>
2020-06-05 23:47:53 -0700

    shallow render component

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import { configure } from 'enzyme'
+import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('<App />', () => {
+  it('should render App', () => {
+    const wrapper = shallow(<App />, {context: {}, disableLifecycleMethods: bool})
+    console.log(wrapper.debug())
+  })
+})
